0818a14f21590ee8b6a212c9848ad19e9d68460e
jm54103 <jatuporn.mabangcru@gmail.com>
2025-10-02 13:44:43 +0700

    Toggle Menu

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDrawerContainer, MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule, MatToolbarRow } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [MatDrawerContainer, MatSidenavModule, MatToolbarModule, MatIconModule, MatDividerModule],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
 export class MainComponent {
+  
+  isOpenedMenu=false;
+
+  onClickToggleMenu(){
+    this.isOpenedMenu=!this.isOpenedMenu;
+  }
+
+
+  onClickMenu(e:HTMLElement)
+  {
+    console.log(`${e.id}`);
+  }
 
 }
