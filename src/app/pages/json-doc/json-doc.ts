512afe44666778c9c038fd69c50cb83bfddde3b6
jm54103 <jatuporn.mabangcru@gmail.com>
2025-10-05 21:02:31 +0700

    JsonObject View

--- src/app/pages/json-doc/json-doc.ts
+++ src/app/pages/json-doc/json-doc.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,10 +8,22 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { DialogAlertComponent } from '../../components/dialogs/dialog-alert.component/dialog-alert.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable, MatTableModule} from '@angular/material/table';
+
+export interface KeyValue {
+  key: string;
+  value: string;
+}
+
+
 
 @Component({
   selector: 'app-json-doc',
-  imports: [           
+  imports: [     
+    JsonPipe,      
+    MatTableModule,
     MatCardModule,
     MatFormFieldModule,
     MatSelectModule,
@@ -26,16 +38,29 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class JsonDoc {
 
+
+  data: KeyValue[] = [
+  
+  ];
+
+  @ViewChild(MatTable) table!: MatTable<KeyValue>;
+
+  readonly dialog = inject(MatDialog);
+  displayedColumns: string[] = ['key', 'value'];
+
   jsonDocForm!:FormGroup; 
   jsonText="";
-  jsonObject:any;
+  jsonObject={};
  
   ObjectName="Document" ;
 
   constructor(
-    private fb: FormBuilder  
+    private fb: FormBuilder ,   
+
   ) 
-  { } 
+  { 
+  
+  } 
   
   ngOnInit(): void {   
     this.build();
@@ -45,51 +70,75 @@ export class JsonDoc {
   {
     this.jsonDocForm=this.fb.group({
       keyName:['', [Validators.required]],   
-      keyValue:['', []],   
+      keyValue:['', [Validators.required]],   
     });
   }
 
-  public rows:string[]=[];
+  keyValueMap = new Map<string, string>();
+
+  setKeyValue(k:string,v:string){
+    this.keyValueMap.set(k,v);
+    let e:KeyValue={key:k,value:v};
+    this.data.push(e);
+    this.table.renderRows();
+  }
+
+  getJsonObject()
+  {
+    // ดึง Key ทั้งหมด
+    const keys = Array.from(this.keyValueMap.keys());
+    this.jsonText=`{ `;
+    keys.forEach(key => {
+      let value=this.keyValueMap.get(key);
+      let row="";
+      switch(value)
+      {
+        case "String":
+          row=`"${key}":null`;
+          break;
+        case "Number":
+          row=`"${key}":0 `;
+          break;
+        case "Array":
+          row=`"${key}":[ ] `;
+          break;
+        case "Object":
+          row=`"${key}":{ }`;
+          break;
+      }
+      this.jsonText+= (keys[0]==key) ? row : `,${row}`;
+    });
+    this.jsonText+=` }`;
+    
+    console.log(this.jsonText);
+    this.jsonObject=JSON.parse(this.jsonText);
+    this.jsonText=JSON.stringify(this.jsonObject,null,2)
+    console.log(this.jsonText);  
+
+  }
+
 
   onClickAdd()
   {
-      const objectName=this.ObjectName;
       const keyName=this.jsonDocForm.controls['keyName'].value;
       const keyValue=this.jsonDocForm.controls['keyValue'].value;
-      let row="";
-      switch(keyValue)
-      {
-        case "String":
-          row=`"${keyName}":null`;
-          break;
-        case "Number":
-          row=`"${keyName}":0 `;
-          break;
-        case "Array":
-          row=`"${keyName}":[ ] `;
-          break;
-        case "Object":
-          row=`"${keyName}":{ }`;
-          break;
-      }
-      this.rows.push(row);
-      this.jsonText=`{ `;
-      let l=this.rows.length;
-      for(let i=0;i<l;i++)
-      {
-        if(i>0)
-        {
-          this.jsonText+=`,${this.rows[i]}`;   
-        }
-        else
-        {
-          this.jsonText+=`${this.rows[i]}`;   
-        }
-             
-      }
-      this.jsonText+=` }`;
-      console.log(this.jsonText);
-      console.log(JSON.stringify(JSON.parse(this.jsonText),null,2));      
 
-     }
+      let key=this.keyValueMap.get(keyName);
+      if(key==null)
+      {
+          this.setKeyValue(keyName,keyValue);
+          this.getJsonObject();          
+      }
+      else
+      {
+        const warn = `The key name '${keyName}' has already key in Json Object.`
+        const dialogConfirmRef = this.dialog.open(DialogAlertComponent,
+        {
+                data: {title:"แจ้งเตือน", message:warn},
+        });
+        console.warn(warn)
+      }
+
+  }
+
 }
