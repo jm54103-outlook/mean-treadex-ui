71006ac8c44dc1ecfe9445e36ffebedd2e4072cf
jm54103 <jatuporn.mabangcru@gmail.com>
2025-10-06 22:30:01 +0700

    Edit JsonObject

--- src/app/pages/json-doc/json-doc.ts
+++ src/app/pages/json-doc/json-doc.ts
@@ -8,11 +8,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 import { DialogAlertComponent } from '../../components/dialogs/dialog-alert.component/dialog-alert.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTable, MatTableModule} from '@angular/material/table';
 
 export interface KeyValue {
+  id: number;
   key: string;
   value: string;
 }
@@ -23,6 +25,7 @@ export interface KeyValue {
   selector: 'app-json-doc',
   imports: [     
     JsonPipe,      
+    MatIconModule,
     MatTableModule,
     MatCardModule,
     MatFormFieldModule,
@@ -38,6 +41,9 @@ export interface KeyValue {
 })
 export class JsonDoc {
 
+  disabledAdd=false;
+  disabledEdit=true;
+  disabledRemove=true;
 
   data: KeyValue[] = [
   
@@ -46,9 +52,9 @@ export class JsonDoc {
   @ViewChild(MatTable) table!: MatTable<KeyValue>;
 
   readonly dialog = inject(MatDialog);
-  displayedColumns: string[] = ['key', 'value'];
+  displayedColumns: string[] = ['id', 'key', 'value'];
 
-  jsonDocForm!:FormGroup; 
+  form!:FormGroup; 
   jsonText="";
   jsonObject={};
  
@@ -68,45 +74,56 @@ export class JsonDoc {
 
   build()
   {
-    this.jsonDocForm=this.fb.group({
+    this.form=this.fb.group({
       keyName:['', [Validators.required]],   
       keyValue:['', [Validators.required]],   
     });
   }
 
-  keyValueMap = new Map<string, string>();
 
-  setKeyValue(k:string,v:string){
-    this.keyValueMap.set(k,v);
-    let e:KeyValue={key:k,value:v};
-    this.data.push(e);
+
+  setKeyValue(i:number,k:string,v:string){
+    
+    let e:KeyValue={id:i, key:k, value:v};
+    let row=this.data.at(i);
+    if(row==null)
+    {    
+      this.data.push(e);
+    }
+    else
+    {
+      row.key=k;
+      row.value=v;     
+    }
+    
     this.table.renderRows();
+    this.getJsonObjectFromArray();
+
   }
 
-  getJsonObject()
+  getJsonObjectFromArray()
   {
-    // ดึง Key ทั้งหมด
-    const keys = Array.from(this.keyValueMap.keys());
     this.jsonText=`{ `;
-    keys.forEach(key => {
-      let value=this.keyValueMap.get(key);
+    this.data.forEach(e=>{
+      let value=e.value;
       let row="";
       switch(value)
       {
         case "String":
-          row=`"${key}":null`;
+          row=`"${e.key}":null`;
           break;
         case "Number":
-          row=`"${key}":0 `;
+          row=`"${e.key}":0 `;
           break;
         case "Array":
-          row=`"${key}":[ ] `;
+          row=`"${e.key}":[ ] `;
           break;
         case "Object":
-          row=`"${key}":{ }`;
+          row=`"${e.key}":{ }`;
           break;
       }
-      this.jsonText+= (keys[0]==key) ? row : `,${row}`;
+      this.jsonText+= (this.data[0].key==e.key) ? row : `,${row}`;
+
     });
     this.jsonText+=` }`;
     
@@ -114,20 +131,20 @@ export class JsonDoc {
     this.jsonObject=JSON.parse(this.jsonText);
     this.jsonText=JSON.stringify(this.jsonObject,null,2)
     console.log(this.jsonText);  
-
   }
-
 
   onClickAdd()
   {
-      const keyName=this.jsonDocForm.controls['keyName'].value;
-      const keyValue=this.jsonDocForm.controls['keyValue'].value;
+      const keyName=this.form.controls['keyName'].value;
+      const keyValue=this.form.controls['keyValue'].value;
 
-      let key=this.keyValueMap.get(keyName);
-      if(key==null)
+      let found=this.data.filter(e=>e.key==keyName).length>0;
+      console.log(`found:${found}`);
+      if(!found)
       {
-          this.setKeyValue(keyName,keyValue);
-          this.getJsonObject();          
+          let id=this.data.length;
+          this.setKeyValue(id,keyName,keyValue);
+          this.getJsonObjectFromArray();          
       }
       else
       {
@@ -140,5 +157,48 @@ export class JsonDoc {
       }
 
   }
+
+
+  selectedIndex=0;
+  onClickEdit()
+  {
+    console.log(`onClickEdit()`)
+  
+    let key = this.form.controls['keyName'].value;
+    let value =  this.form.controls['keyValue'].value;
+ 
+    this.setKeyValue(this.selectedIndex, key, value);
+   
+  }
+
+  onClickRemove()
+  {
+    this.form.reset();
+    let Index=this.data.findIndex(e=>e.id==this.selectedIndex);
+    console.log(`this.data.findIndex:${Index}`);   
+    this.data=this.data.filter(e=>e.id!=this.selectedIndex);
+    console.log(`this.data.length:${this.data.length}`);
+    this.getJsonObjectFromArray();     
+    this.table.renderRows();
+    
+  }
+
+  onClickSelectedRow(kv:any)
+  {
+    console.log(`onClickSelectedRow(${kv.id})`)
+
+
+    this.form.reset();
+
+    this.selectedIndex=kv.id;
+    this.form.controls['keyName'].setValue(kv.key);
+    this.form.controls['keyValue'].setValue(kv.value);
+    
+    this.disabledAdd=true;
+    this.disabledEdit=false;
+    this.disabledRemove=false;
+  
+  }
+
 
 }
